1be479d0386228ef8ebc112363a9316666748fc8
arda5566q <108197943+arda5566q@users.noreply.github.com>
2023-05-31 21:07:57 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
-        'Have you lost your way, kiddo?',
-        'Damn it! This dead-end road.',
-        'The Aurors blocked this road, Harry!'
+        'Yolunu mu kaybettin, evlat?',
+        'Lanet olsun! Burası çıkmaz yol.',
+        'Sihirbazlar bu yolu kapattı, Harry!'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
