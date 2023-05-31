c435affd58adf823f6834f829c28dc9d15971c91
arda5566q <108197943+arda5566q@users.noreply.github.com>
2023-05-31 21:28:35 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -43,7 +43,7 @@ export default function Commands() {
          Bot Commands
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the Astra Bot.
+        Lucıfer Bot'un komutları hakkında bilgi alabilirsiniz.
         </p>
 
         <div>
@@ -92,21 +92,21 @@ export default function Commands() {
   
   <Link href={"https://bit.ly/3PHDjyC"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite Astra</a>
+Lucıfer'ı Ekle</a>
   </Link>
 
   <div className="py-10"></div>
   
   <Link href={"https://discord.gg/ZcDprCrFTn"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Support Server</a>
+Destek Sunucusu</a>
   </Link>
 
   <div className="py-10"></div>
   
   <Link href={"https://bit.ly/3LYzaDe"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Astra</a>
+Lucıfer'a Oy Ver</a>
   </Link>
 </div>
 
